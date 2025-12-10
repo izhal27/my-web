bb795f3731e6cea4dd35c1c24e03929bb560dfda
Risal Walangadi <risal.walangadi@gmail.com>
2025-12-11 06:45:56 +0800

    delete next export in build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
-  async headers() {
-    return [
-      {
-        source: '/:all*(svg|jpg|png)',
-        locale: false,
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=9999999999, must-revalidate',
-          },
-        ],
-      },
-    ];
+  output: "export",
+  images: {
+    unoptimized: true,
   },
 };
